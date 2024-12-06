d9eb458fe8e38e29e11b33249ef593cff7c1e5c0
Jonatas <delimajonatas875@gmail.com>
2024-12-06 19:40:33 -0300

    build: plugin navic-nvim

---
+++ lua/plugins/navic.lua
@@ -0,0 +1,13 @@
+return {
+  "SmiteshP/nvim-navic",
+  config = function()
+    local icons = require("icons")
+    require("nvim-navic").setup({
+      highlight = false,          -- Realce para os símbolos na winbar
+      separator = " > ",          -- Separador entre os símbolos
+      depth_limit = 0,            -- Número máximo de símbolos exibidos (0 = sem limite)
+      depth_limit_indicator = "...", -- Indicador quando o limite for atingido
+      icons = icons.kind,
+    })
+  end,
+}
