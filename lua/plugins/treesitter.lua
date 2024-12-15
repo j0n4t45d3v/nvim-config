fd74d581009dd1d7369e17a48904f1fa0f278f53
Jonatas <delimajonatas875@gmail.com>
2024-12-15 11:39:00 -0300

    feat: surrond.nvim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,6 +11,33 @@ return {
       modules = {},
       ensure_installed = {},
       ignore_install = {},
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Move automaticamente para o próximo texto correspondente
+          keymaps = {
+            -- Definir text objects para funções
+            ["af"] = "@function.outer", -- A função inteira
+            ["if"] = "@function.inner", -- Apenas o conteúdo da função
+
+            -- Definir text objects para classes
+            ["ac"] = "@class.outer", -- A classe inteira
+            ["ic"] = "@class.inner", -- Apenas o conteúdo da classe
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,       -- Adiciona os movimentos ao jumplist
+          goto_next_start = {
+            ["]m"] = "@function.outer", -- Próximo início de função
+            ["]c"] = "@class.outer", -- Próximo início de classe
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer", -- Função anterior
+            ["[c"] = "@class.outer", -- Classe anterior
+          },
+        },
+      },
     })
   end,
 }
