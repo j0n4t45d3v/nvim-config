83c06ff49f9ae67716f61962e5233c0905361c5c
Jonatas <delimajonatas875@gmail.com>
2024-12-21 11:23:10 -0300

    fix: runtimes failed and not found main class

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,6 +1,6 @@
 local status, jdtls = pcall(require, "jdtls")
 if not status then
-  return
+	return
 end
 
 local home = os.getenv("HOME")
@@ -11,7 +11,7 @@ local workspace_jdtls_dir = workspace_jdtls_path .. project_name
 local os_config = "linux"
 
 if vim.fn.has("mac") == 1 then
-  os_config = "mac"
+	os_config = "mac"
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -24,174 +24,120 @@ local bundles = {}
 local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
 vim.list_extend(bundles, vim.split(vim.fn.glob(mason_path .. "packages/java-test/extension/server/*.jar"), "\n"))
 vim.list_extend(
-  bundles,
-  vim.split(
-    vim.fn.glob(mason_path .. "packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar"),
-    "\n"
-  )
+	bundles,
+	vim.split(
+		vim.fn.glob(mason_path .. "packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar"),
+		"\n"
+	)
 )
 
 local config = {
-  -- command start lsp java configured
-  cmd = {
-    "java",
-    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-    "-Dosgi.bundles.defaultStartLevel=4",
-    "-Declipse.product=org.eclipse.jdt.ls.core.product",
-    "-Dlog.protocol=true",
-    "-Dlog.level=ALL",
-    "-Xms1g",
-    "--add-modules=ALL-SYSTEM",
-    "--add-opens",
-    "java.base/java.util=ALL-UNNAMED",
-    "--add-opens",
-    "java.base/java.lang=ALL-UNNAMED",
-    "-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
-    "-jar",
-    vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-    "-configuration",
-    home .. "/.local/share/nvim/mason/packages/jdtls/config_" .. os_config,
-    "-data",
-    workspace_jdtls_dir,
-  },
-  root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
-  capabilities = capabilities,
+	cmd = {
+		"java",
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.protocol=true",
+		"-Dlog.level=ALL",
+		"-Xms1g",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens",
+		"java.base/java.util=ALL-UNNAMED",
+		"--add-opens",
+		"java.base/java.lang=ALL-UNNAMED",
+		"-javaagent:" .. home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
+		"-jar",
+		vim.fn.glob(home .. "/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+		"-configuration",
+		home .. "/.local/share/nvim/mason/packages/jdtls/config_" .. os_config,
+		"-data",
+		workspace_jdtls_dir,
+	},
+	root_dir = require("jdtls.setup").find_root({ ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }),
+	capabilities = capabilities,
 
-  settings = {
-    java = {
-      eclipse = {
-        downloadSources = true,
-      },
-      configuration = {
-        updateBuildConfiguration = "interactive",
-        runtimes = {
-          {
-            name = "JavaSE-1.8-TEM",
-            path = home .. "/.sdkman/candidates/java/8.0.412-tem",
-          },
-          {
-            name = "Java-21-TEM",
-            path = home .. "/.sdkman/candidates/java/21.0.2-tem",
-          },
-          {
-            name = "Java-21-GRAAL",
-            path = home .. "/.sdkman/candidates/java/21.0.2-graal",
-          },
-          {
-            name = "Java-17-TEM",
-            path = home .. "/.sdkman/candidates/java/17.0.11-tem",
-          },
-        },
-      },
-      maven = {
-        downloadSources = true,
-      },
-      referencesCodeLens = {
-        enabled = true,
-      },
-      references = {
-        includeDecompiledSources = true,
-      },
-      inlayHints = {
-        parameterNames = {
-          enabled = "all", -- literals, all, none
-        },
-      },
-      format = {
-        enabled = false,
-      },
-    },
-    signatureHelp = { enabled = true },
-    extendedClientCapabilities = extended_client_capabilities,
-  },
-  init_options = {
-    bundles = bundles,
-    classpath = home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
-  },
+	settings = {
+		java = {
+			eclipse = {
+				downloadSources = true,
+			},
+			configuration = {
+				updateBuildConfiguration = "interactive",
+			},
+			maven = {
+				downloadSources = true,
+			},
+			referencesCodeLens = {
+				enabled = true,
+			},
+			references = {
+				includeDecompiledSources = true,
+			},
+			contentProvider = {
+				preferred = "fernflower",
+			},
+			format = {
+				enabled = true,
+			},
+			completion = {
+				-- When using an unimported static method, how should the LSP rank possible places to import the static method from
+				favoriteStaticMembers = {
+					"org.hamcrest.MatcherAssert.assertThat",
+					"org.hamcrest.Matchers.*",
+					"org.hamcrest.CoreMatchers.*",
+					"org.junit.jupiter.api.Assertions.*",
+					"java.util.Objects.requireNonNull",
+					"java.util.Objects.requireNonNullElse",
+					"org.mockito.Mockito.*",
+				},
+				-- Try not to suggest imports from these packages in the code action window
+				filteredTypes = {
+					"com.sun.*",
+					"io.micrometer.shaded.*",
+					"java.awt.*",
+					"jdk.*",
+					"sun.*",
+				},
+				-- Set the order in which the language server should organize imports
+				importOrder = {
+					"java",
+					"jakarta",
+					"javax",
+					"com",
+					"org",
+				},
+			},
+		},
+		signatureHelp = { enabled = true },
+		extendedClientCapabilities = extended_client_capabilities,
+	},
+	init_options = {
+		bundles = bundles,
+		classpath = home .. "/.local/share/nvim/mason/packages/jdtls/lombok.jar",
+	},
 }
 
 config["on_attach"] = function(client, bufnr)
-  local _, _ = pcall(vim.lsp.codelens.refresh)
-  require("jdtls").setup_dap({
-    hotcodereplace = "auto",
-    config_overrides = {},
-  })
+	local _, _ = pcall(vim.lsp.codelens.refresh)
+	require("jdtls").setup_dap({
+		hotcodereplace = "auto",
+		config_overrides = {},
+	})
+	require("core.lsp").default_on_attach(client, bufnr)
 
-  if client.server_capabilities.documentSymbolProvider then
-    require("nvim-navic").attach(client, bufnr)
-  end
-  -- Helper para facilitar os mapeamentos
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
-  buf_set_keymap(
-    "n",
-    "gd",
-    "<cmd>lua require('telescope.builtin').lsp_definitions()<CR>",
-    { noremap = true, silent = true, desc = "Go to definition" }
-  )
+	local status_ok, jdtls_dap = pcall(require, "jdtls.dap")
+	if status_ok then
+		jdtls_dap.setup_dap_main_class_configs()
+	end
 
-  -- Go-to declaração
-  buf_set_keymap(
-    "n",
-    "gD",
-    "<cmd>lua vim.lsp.buf.declaration()<CR>",
-    { noremap = true, silent = true, desc = "Go to declaration" }
-  )
-
-  -- Go-to implementação
-  buf_set_keymap(
-    "n",
-    "gi",
-    "<cmd>lua require('telescope.builtin').lsp_implementations()<CR>",
-    { noremap = true, silent = true, desc = "Go to implementation" }
-  )
-
-  -- Go-to tipo
-  buf_set_keymap(
-    "n",
-    "gt",
-    "<cmd>lua require('telescope.builtin').lsp_type_definitions()<CR>",
-    { noremap = true, silent = true, desc = "Go to type definition" }
-  )
-
-  buf_set_keymap(
-    "n",
-    "<leader>lr",
-    "<cmd>lua vim.lsp.buf.rename()<CR>",
-    { noremap = true, silent = true, desc = "Rename" }
-  )
-  -- Mostrar referências
-  buf_set_keymap(
-    "n",
-    "gr",
-    "<cmd>lua require('telescope.builtin').lsp_references()<CR>",
-    { noremap = true, silent = true, desc = "Show references" }
-  )
-
-  -- Hover (ajuda)
-  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", { noremap = true, silent = true, desc = "Helper" })
-
-  -- Exibir assinatura (documentação inline)
-  buf_set_keymap(
-    "n",
-    "<leader>hs",
-    "<cmd>lua vim.lsp.buf.signature_help()<CR>",
-    { noremap = true, silent = true, desc = "Show signature help" }
-  )
+	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+		pattern = { "*.java" },
+		callback = function()
+			local _, _ = pcall(vim.lsp.codelens.refresh)
+		end,
+	})
+	require("core.auto_commands.skeleton_java")
 end
-
-local status_ok, jdtls_dap = pcall(require, "jdtls.dap")
-if status_ok then
-  jdtls_dap.setup_dap_main_class_configs()
-end
-
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = { "*.java" },
-  callback = function()
-    local _, _ = pcall(vim.lsp.codelens.refresh)
-  end,
-})
 
 -- Started jdtls
 require("jdtls").start_or_attach(config)
