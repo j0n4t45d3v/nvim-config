8787e68afe0cd6630f1c89d0e8f5f3178986fb94
Jonatas <delimajonatas875@gmail.com>
2024-12-21 11:26:54 -0300

    chore: remove trash

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,11 +3,7 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   event = "bufwinenter",
   config = function()
-    local navic = require("nvim-navic")
     require("lualine").setup({
-      options = {
-        theme = "lackluster",
-      },
       sections = {
         lualine_x = {
           {
