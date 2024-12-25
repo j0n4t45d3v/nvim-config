03f6c97264db3624b32c2de897c5d02a1d21fa49
Jonatas <delimajonatas875@gmail.com>
2024-12-25 12:03:38 -0300

    fix: bug move keys h and l no navigate in file

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,42 +1,52 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  config = function()
-    local nvim_tree = require("nvim-tree")
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-    nvim_tree.setup({
-      hijack_netrw = true,
-      update_cwd = true,
-      actions = {
-        open_file = {
-          quit_on_open = true,
-        },
-      },
-      view = {
-        width = 30,
-        side = "left",
-      },
-      renderer = {
-        icons = {
-          show = {
-            file = true,
-            folder = true,
-            folder_arrow = true,
-            git = true,
-          },
-        },
-      },
-      git = {
-        enable = true,
-      },
-      filters = {
-        dotfiles = false,
-      },
-    })
+	"nvim-tree/nvim-tree.lua",
+	config = function()
+		local nvim_tree = require("nvim-tree")
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+		nvim_tree.setup({
+			hijack_netrw = true,
+			update_cwd = true,
+			actions = {
+				open_file = {
+					quit_on_open = true,
+				},
+			},
+			view = {
+				width = 30,
+				side = "left",
+			},
+			renderer = {
+				icons = {
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = true,
+						git = true,
+					},
+				},
+				group_empty = true,
+			},
+			git = {
+				enable = true,
+			},
+			filters = {
+				dotfiles = false,
+			},
+			on_attach = function(bufnr)
+				local api = require("nvim-tree.api")
 
-    local opt = { noremap = true, silent = true, nowait = true }
-    local api = require("nvim-tree.api")
-    vim.keymap.set("n", "h", api.node.navigate.parent_close, opt)
-    vim.keymap.set("n", "l", api.node.open.edit, opt)
-  end,
+				local function opts(desc)
+					return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+				end
+
+				-- default mappings
+				api.config.mappings.default_on_attach(bufnr)
+
+				-- custom mappings
+				vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close"))
+				vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+			end,
+		})
+	end,
 }
