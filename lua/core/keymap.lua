8ba0d17d3f5ec1fc5cafaffc316808fe9c3d168a
Jonatas <delimajonatas875@gmail.com>
2025-01-20 20:16:18 -0300

    chore: new keybinds

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -12,7 +12,7 @@ key.set("n", "<A-k>", "<cmd>m .-2<cr>==", { desc = "Move line up" })
 
 key.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move block down" })
 key.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move block up" })
-
+key.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- MOVING IN WINDOWS
 key.set("n", "<C-l>", "<C-w>l", opt("Go to left window"))
 key.set("n", "<C-h>", "<C-w>h", opt("Go to right window"))
@@ -24,79 +24,30 @@ key.set("n", "<leader>sq", "<C-w>q", opt("Switch for down window"))
 key.set("n", "<leader>sv", "<C-w>v", opt("Split vertical window"))
 key.set("n", "<leader>ss", "<C-w>s", opt("Split horizontal window"))
 
-key.set("n", "<M-h>", "<C-w><", opt("Decrement width window"))
-key.set("n", "<M-l>", "<C-w>>", opt("Increment width windows"))
-key.set("n", "<M-k>", "<C-w>+", opt("Increment height window"))
-key.set("n", "<M-j>", "<C-w>-", opt("Decrement height window"))
+key.set("n", "<M-S-h>", "<C-w><", opt("Decrement width window"))
+key.set("n", "<M-S-l>", "<C-w>>", opt("Increment width windows"))
+key.set("n", "<M-S-k>", "<C-w>+", opt("Increment height window"))
+key.set("n", "<M-S-j>", "<C-w>-", opt("Decrement height window"))
+
+
+key.set("n", "<leader>sc", "<cmd>lua require('blanket').start()<cr>", opt("[S]how [C]overage"))
+key.set("n", "<leader>ec", "<cmd>lua require('blanket').stop()<cr>", opt("[E]xit [C]overage"))
 
 -- NVIM TREE
 key.set("n", "<leader>e", ":NvimTreeFindFileToggle<CR>", opt("Open/Close explore"))
 
 -- TELESCOPE
 
-key.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opt("Find Files"))
+key.set("n", "<leader>ff", "<cmd>Telescope find_files theme=dropdown<cr>", opt("Find Files"))
 key.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", opt("Find Recente Files"))
-key.set("n", "<leader>fl", "<cmd>Telescope live_grep<cr>", opt("Telescope live grep"))
+key.set("n", "<leader>fl", "<cmd>Telescope live_grep theme=dropdown<cr>", opt("Telescope live grep"))
 key.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", opt("Telescope show keybinds"))
-key.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", opt("Telescope buffers"))
+key.set("n", "<leader>fb", "<cmd>Telescope buffers theme=dropdown<cr>", opt("Telescope buffers"))
 key.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", opt("Telescope help tags"))
-key.set("n", "<leader>fc", "<cmd>Telescope colorscheme<cr>", opt("List colorschemes"))
+key.set("n", "<leader>fc", "<cmd>Telescope colorscheme theme=ivy<cr>", opt("List colorschemes"))
 -- FORMAT
 key.set("n", "<leader>lf", vim.lsp.buf.format, { desc = "Format code" })
 key.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = "Code actions" })
-
--- Mapear no modo normal
-key.set(
-  "n",
-  "<leader>Jo",
-  "<Cmd>lua require'jdtls'.organize_imports()<CR>",
-  { noremap = true, silent = true, desc = "Organize Imports" }
-)
-key.set(
-  "n",
-  "<leader>Jv",
-  "<Cmd>lua require('jdtls').extract_variable()<CR>",
-  { noremap = true, silent = true, desc = "Extract Variable" }
-)
-key.set(
-  "n",
-  "<leader>JC",
-  "<Cmd>lua require('jdtls').extract_constant()<CR>",
-  { noremap = true, silent = true, desc = "Extract Constant" }
-)
-key.set(
-  "n",
-  "<leader>Jt",
-  "<Cmd>lua require'jdtls'.test_nearest_method()<CR>",
-  { noremap = true, silent = true, desc = "Test Method" }
-)
-key.set(
-  "n",
-  "<leader>JT",
-  "<Cmd>lua require'jdtls'.test_class()<CR>",
-  { noremap = true, silent = true, desc = "Test Class" }
-)
-key.set("n", "<leader>Ju", "<Cmd>JdtUpdateConfig<CR>", { noremap = true, silent = true, desc = "Update Config" })
-
--- Mapear no modo visual
-key.set(
-  "v",
-  "<leader>Cv",
-  "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>",
-  { noremap = true, silent = true, desc = "Extract Variable" }
-)
-key.set(
-  "v",
-  "<leader>Cc",
-  "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>",
-  { noremap = true, silent = true, desc = "Extract Constant" }
-)
-key.set(
-  "v",
-  "<leader>Cm",
-  "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>",
-  { noremap = true, silent = true, desc = "Extract Method" }
-)
 
 --TESTS
 key.set("n", "<leader>tr", function()
@@ -119,7 +70,7 @@ key.set("n", "<leader>td", function()
   require("neotest").run.run({ vim.fn.expand("%"), strategy = "dap", suite = false })
 end, opt("Run Tests in file debug mode"))
 
-key.set("n", "<leader>tr", function()
+key.set("n", "<leader>tp", function()
   require("neotest").run.stop()
 end, opt("Stop Test"))
 
@@ -130,3 +81,4 @@ end, opt("Output test"))
 key.set("n", "<leader>ts", function()
   require("neotest").summary.toggle()
 end, opt("Summary tests"))
+
