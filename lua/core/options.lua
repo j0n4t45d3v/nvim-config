2cb5e812390217887e5dfac5c8d799161713417a
Jonatas <delimajonatas875@gmail.com>
2025-01-20 20:17:02 -0300

    chore: more options

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -6,6 +6,8 @@ set.number = true
 
 set.mouse = "a"
 set.autoindent = true
+set.breakindent = true
+set.undofile = true
 set.smartindent = true
 set.smarttab = true
 set.cursorline = true
@@ -24,15 +26,29 @@ set.termguicolors = true
 set.background = "dark"
 set.colorcolumn = "80"
 set.fillchars = { eob = " " }
+vim.o.showmode = false
+--[[ set.list = true
+set.listchars = { tab = '» ', trail = '·', nbsp = '␣' } ]]
+set.scrolloff = 10
+set.hlsearch = true
 
 vim.loader.enable()
 
 vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#ffffff", fg = "NONE", underline = false })
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = vim.api.nvim_create_augroup("highlight_yank", {}),
-  desc = "Hightlight selection on yank",
-  pattern = "*",
-  callback = function()
-    vim.highlight.on_yank({ higroup = "IncSearch", timeout = 20 })
-  end,
+	group = vim.api.nvim_create_augroup("highlight_yank", {}),
+	desc = "Hightlight selection on yank",
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 20 })
+	end,
 })
+
+-- Set kitty terminal padding to 0 when in nvim
+vim.cmd([[
+  augroup kitty_mp
+  autocmd!
+  au VimLeave * :silent !kitty @ set-spacing padding=default margin=default
+  au VimEnter * :silent !kitty @ set-spacing padding=0 margin=0 3 0 3
+  augroup END
+]])
