dec9e36ec7366125eaf94a36808c2d9fdf1021b6
Jonatas <delimajonatas875@gmail.com>
2025-08-21 20:23:48 -0300

    feat: add indent-blankline plugin

---
+++ lua/after/plugin/indent_line.lua
@@ -0,0 +1,10 @@
+return {
+  { -- Add indentation guides even on blank lines
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    main = "ibl",
+    opts = {
+      indent = { char = "┊" },
+    },
+  },
+}
